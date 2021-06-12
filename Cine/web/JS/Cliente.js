5f16bb23d97e1a7dfe6a714f4cd7a623803ed7ee
Rando <boyrojas97@gmail.com>
2021-06-11 19:20:02 -0600

    USER y CLIENTE JS

--- Cine/web/JS/Cliente.js
+++ Cine/web/JS/Cliente.js
@@ -6,39 +6,35 @@
 var url="http://localhost:8080/Cine/";
 
 
-
-
-
-
-
 function register(){
-         console.log("LOGIN");
+         console.log("REGISTER");
         //if (!loginValidar()) return;
         cliente = {
-            id: $("#id").val(),
-            password: $("#password").val(),
+            id: $("#idR").val(),
+            password: $("#passwordR").val(),
             nombre:$("#nombre").val(),
             apellidos :$("#apellidos").val(),
             numero_cuenta :$("#numero_cuenta").val(),
-            type = "CLIENTE"
+            type: "CLIENTE"
         };
         usuario = {
-            id: $("#id").val(),
-            password: $("#password").val(),
-            type = "CLIENTE"
+            id: "DEFAULT",
+            password: "DEFAULT",
+            type: "DEFAULT"
         };
         console.log(cliente);
-        let request = new Request(url+'api/cliente/register', {method: 'POST', headers: { 'Content-Type': 'application/json'},body: JSON.stringify(cliente)});
+        console.log(usuario);
+        let request = new Request(url+'api/usuario/register', {method: 'POST', headers: { 'Content-Type': 'application/json'},body: JSON.stringify(cliente)});
         (async ()=>{
             const response = await fetch(request);
-            if (!response.ok) {errorMessage(response.status,$("#loginDialog #errorDiv"));return;}
+            if (!response.ok) {errorMessage(response.status,$("#loginDialog #errorDiv"));return false;}
             usuario = await response.json();
             sessionStorage.setItem('Usuario', JSON.stringify(usuario));
             $('#loginDialog').modal('hide');            
            switch(cliente.type){
                case 'ADMINISTRATOR': console.log("LOGIN ADMINISTRATOR");//document.location = url+"listado.html"; 
                    break;
-               case 'CLIENTE': console.log("LOGIN CLIENTE");//document.location = url+"about.html"; 
+               case 'CLIENTE': console.log("LOGIN REGISTER CLIENTE");//document.location = url+"about.html"; 
                    break;
            }                           
         })(); 
